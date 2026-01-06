0d2f03177cb56b2deacb81778a95a46d6ed5b4aa
kshitij mudey <kshitijsep0604@gmail.com>
2026-01-06 07:21:07 +0000

    in this  i have created test for the staging models

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,11 +1,24 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with 
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+source as (
 
-from {{ source('stripe', 'payment') }}
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount / 100 as amount,
+        created as created_at,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
